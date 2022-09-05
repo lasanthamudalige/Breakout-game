2bdbe26e3a22c23778d2d343862ee65c3826dbc2
Lasantha Mudalige <lasantha1130@gmail.com>
2022-09-05 13:38:32 +0530

    add paddle to main file

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from turtle import Screen
-
-screen = Screen()
+from paddle import Paddle
 
 
 def main():
-    ...
+    screen = Screen()
+    screen.bgcolor("black")
+    screen.setup(800, 600)
+    screen.title("Breakout game")
+
+    paddle = Paddle(position=(0, -250))
+
+    # Move left and right when user press left/right arrow keys or a,d keys on the keyboard
+    screen.listen()
+    screen.onkey(paddle.go_left, "a")
+    screen.onkey(paddle.go_right, "d")
+    screen.onkey(paddle.go_left, "Left")
+    screen.onkey(paddle.go_right, "Right")
 
     screen.exitonclick()
 
---
+++ paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__(shape="square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.tilt(90)
+        self.goto(position)
+
+    def go_left(self):
+        new_x = self.xcor() - 40
+        self.goto(new_x, self.ycor())
+
+    def go_right(self):
+        new_x = self.xcor() + 40
+        self.goto(new_x, self.ycor())
