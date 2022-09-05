d3564c4fc695c8173606fe99163848f5bc6a2b04
Lasantha Mudalige <lasantha1130@gmail.com>
2022-09-05 18:16:52 +0530

    complete ball file and add collision with wall and the paddle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+import time
+
 
 def main():
     screen = Screen()
@@ -18,7 +20,19 @@ def main():
     screen.onkey(paddle.go_left, "Left")
     screen.onkey(paddle.go_right, "Right")
 
+    game_is_on = True
+    while game_is_on:
+        time.sleep(ball.move_speed)
+        screen.update()
+        ball.move()
 
+        # detect the collision with wall
+        if ball.xcor() > 380 or ball.xcor() < -380:
+            ball.bounce_x()
+
+        # detect collision with paddle
+        if ball.distance(paddle) < 20 and ball.xcor() > -320:
+            ball.bounce_y()
 
     screen.exitonclick()
 
