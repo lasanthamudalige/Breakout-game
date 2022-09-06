4862a59c263aeb22e33151c891a9eab8e9354244
Lasantha Mudalige <lasantha1130@gmail.com>
2022-09-06 13:20:17 +0530

    add bricks to the screen

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import random
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
-from bricks import Bricks
+from brick import Brick
 import time
 
 
@@ -10,10 +11,30 @@ def main():
     screen.bgcolor("black")
     screen.setup(800, 600)
     screen.title("Breakout game")
+    screen.tracer(0)
 
     paddle = Paddle(position=(0, -250))
     ball = Ball()
-    bricks = Bricks((-280, 220))
+
+    bricks = 0
+    x = -360
+    y = 220
+    for i in range(45):
+        colors = ["red", "lime", "blue", "yellow", "cyan", "magenta", "silver", "grey", "maroon", "olive",
+                  "green", "purple", "teal", "navy", "coral", "salmon", "gold", "khaki", "olive", "green", "teal",
+                  "aqua", "turquoise", "navy", "indigo", "purple", "thistle", "plum", "violet", "orchid", "pink",
+                  "beige", "wheat", "sienna", "peru", "tan", "moccasin", "linen", "mint cream",
+                  "slate gray", "lavender", "floral white", "alice blue", "ghost white", "honeydew", "ivory", "azure",
+                  "snow"]
+        random_color = random.choice(colors)
+        Brick((x, y), random_color)
+        x += 90
+        bricks += 1
+
+        if bricks % 9 == 0:
+            x = -360
+            y -= 30
+
 
     # Move left and right when user press left/right arrow keys or a,d keys on the keyboard
     screen.listen()
@@ -35,9 +56,12 @@ def main():
         # detect collision with paddle
         if ball.distance(paddle) < 35 and ball.xcor() > -320:
             ball.bounce_y()
+
         # detect collision with top wall
         if ball.ycor() > 280:
             ball.bounce_y()
+
+        # if ball.distance()
 
     screen.exitonclick()
 
